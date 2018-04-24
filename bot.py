8baff9705cce408499951dbfc7ca1ee12374df3a
topias33 <38688365+topias33@users.noreply.github.com>
2018-04-24 19:30:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,29 +5,36 @@ import datetime
 import telepot
 import subprocess
 import shlex
-import unicodedata
 
 def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
-    
-    unicodedata.normalize('NFKD', command).encode('ascii','ignore')
 
     print 'Got command: %s' % command
     
-    if command.split()[0] == "/bash":
-        bash(command)
-        #bot.sendMessage(chat_id, bash(command))
-    if command == '/hei':
+    tag = command.split()[0]
+    args = arguments(command)
+    
+    if tag == "/bash":
+        bot.sendMessage(chat_id, bash(args))
+    elif tag == "/bash":
+        bot.sendMessage(chat_id, math(args))
+    elif tag == "/hei":
         bot.sendMessage(chat_id, 'Hello World from githubbbb')
 
-def bash(command):
-    args = shlex.split(command)
-    args.pop(0) # .pop(0) removes '/bash'
-    print args
+def bash(args):
     str = subprocess.check_output(args, stderr=subprocess.STDOUT)
     print str
-    #return str
+    return str
+
+def math(args):
+    print "working on it"
+    
+def arguments(command):
+    args = shlex.split(command)
+    args.pop(0) # .pop(0) removes ex. '/bash'
+    print args
+    return args
             
 
 bot = telepot.Bot('555366873:AAG6ZRWJjmSFwuYFEweQDZCfiSqtECvpt9M')
