f1f638251f9fac8d89e2989be22555a44208f5cb
topias33 <38688365+topias33@users.noreply.github.com>
2018-04-24 13:16:05 +0300

    Update Vattu_bot.py

--- Vattu_bot.py
+++ Vattu_bot.py
@@ -1,1 +1,21 @@
+import sys
+import time
+import random
+import datetime
+import telepot
 
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+
+    print 'Got command: %s' % command
+
+    if command == '/hei':
+       bot.sendMessage(chat_id, 'Hello World from github')
+
+bot = telepot.Bot('555366873:AAG6ZRWJjmSFwuYFEweQDZCfiSqtECvpt9M')
+bot.message_loop(handle)
+print 'I am listening...'
+
+while 1:
+     time.sleep(10)
