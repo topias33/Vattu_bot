8468dc959aba14700fefbf73df17d9ce5014f8a3
imuuu <33964608+imuuu@users.noreply.github.com>
2018-05-03 02:18:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -104,6 +104,8 @@ def handle(msg):
         bot.sendMessage(chat_id, 'Miten menee?')
     
     elif tag =='fwiki':
+        print(args)
+        print('helloo')
         codes=wikiCall.getAbbreviation()
         argsS=args.split(' ')
         if argsS[1] in codes:
