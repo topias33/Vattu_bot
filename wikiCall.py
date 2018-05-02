0aebc445a1237303d3dfe0ecd939fa6fc58b07ff
imuuu <33964608+imuuu@users.noreply.github.com>
2018-05-03 02:06:22 +0300

    Add files via upload

---
+++ wikiCall.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup
+import requests
+def getAbbreviation():
+    source = requests.get('http://sustainablesources.com/resources/country-abbreviations/').text
+    soup = BeautifulSoup(source, 'lxml')
+    countryCodeSoup=soup.find('div',class_="post-bodycopy clearfix")
+    codes=[]
+    codesX=[]
+    codesXX=[]
+    for textii in countryCodeSoup.find_all('tr'):
+        codes.append(textii.text)
+    for i in range(len(codes)):
+        list=codes[i].split('\n')
+        for ii in list:
+            codesX.append(ii)
+    for iii in codesX:
+        if iii!='' and len(iii)==2:
+            if iii.isupper()==False:
+                codesXX.append(iii)
+    codesXX.insert(0,'en')
+   
+    return codesXX
+
+def wikiSearch(searchString,languageString=None):
+    #return string
+    try:
+        search=searchString
+        if languageString!=None:
+            language=languageString
+        else:
+            language='en'
+        try:
+            source = requests.get('https://'+language+'.wikipedia.org/wiki/'+search).text
+            soup = BeautifulSoup(source, 'lxml')
+        except:
+            source = requests.get('https://en.wikipedia.org/wiki/'+search).text
+            soup = BeautifulSoup(source, 'lxml')
+        
+        mainP=soup.find('p').text
+        removeList=[]
+        
+        for i in range(100):
+            ii="["+str(i)+"]"
+            removeList.append(ii)
+        
+        for item in removeList:
+            mainP=mainP.replace(item,'')
+        print(len(mainP))
+    except:
+        mainP=''   
+    
+    if len(mainP)<15:
+        mainP="Page didn't found"
+    elif mainP[-3:len(mainP)]=="to:":
+        mainP="Too many similarities, please give more specific argument"
+        
+    
+    return mainP
+
