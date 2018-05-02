355999d7179d58b9b1020ac00f504e4bc0e0dc11
topias33 <38688365+topias33@users.noreply.github.com>
2018-05-02 17:27:43 +0300

    Update TempIlldeletesoon.py

--- TempIlldeletesoon.py
+++ TempIlldeletesoon.py
@@ -1,11 +1,12 @@
 import sys
 import telepot
 
-#when running pass in the token as the first parameter e.g. python3.4 file.py token
+# Pass in the token as the first parameter. 
+# e.g. python3.4 file.py token
 TOKEN = sys.argv[1] 
 
 def handle(msg):
-
+    # Bot fuctionality
 
 bot = telepot.Bot(TOKEN)
 bot.message_loop(handle)
