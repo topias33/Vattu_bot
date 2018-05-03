a9819dbf119605b95f5d0cea35db317b01c45525
topias33 <38688365+topias33@users.noreply.github.com>
2018-05-03 19:22:39 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -26,7 +26,7 @@ def translate(args):
     webpage = web_byte.decode('utf-8')
 
     soup = BeautifulSoup(webpage, "html.parser")
-    soup = soup.find("span", {'lang': language})
+    soup = soup.find("span", {'id': 'result_box', 'class': 'short_text'})
     print(soup)
     soup = soup.find("span")
     print(soup)
