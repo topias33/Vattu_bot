41d30379386ea86ea6b720954c6783fd0ed958a6
topias33 <38688365+topias33@users.noreply.github.com>
2018-05-04 20:25:48 +0300

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -19,6 +19,7 @@ def quiz_start(questions, amount = 1):
 def quiz_next():
     global game_questions, i, answer
     if i < len(game_questions):
+        print(game_questions)
         question, answer = game_questions[i].rsplit(' ? ',1)
         i += 1
         return question
