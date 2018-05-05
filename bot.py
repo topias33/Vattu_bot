6d4a85b8309055a666e9b471254d98ace24aecb0
topias33 <38688365+topias33@users.noreply.github.com>
2018-05-05 21:58:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -338,6 +338,7 @@ def quiz_game(guess, user):
         bot_print('Rounds: '+str(num_rounds))
         bot_print('Players: '+str(num_players))
         gameList = quiz.quiz_start(read_file('quiz_questions'), num_rounds)
+        print(gameList)
     
     elif guess == 'stop':
         qbool = False
