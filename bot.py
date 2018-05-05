3e5f53a61524333b4ed30fd4fa281f444bd4310b
topias33 <38688365+topias33@users.noreply.github.com>
2018-05-05 13:33:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -322,11 +322,7 @@ def quiz_game(guess, user):
     
     ready = False
     
-    gen = (key for key, value in userdict.items() if value)
-    if gen:
-        players = [for key in gen]
-    else:
-        players = []
+    players = (key for key, value in userdict.items())
         
     if not qbool:
         if len(players) >= num_players:
@@ -354,7 +350,7 @@ def quiz_game(guess, user):
                 ready = True
             else:
                 qguesses -= 1
-                gen = (key for key, value in userdict.items() if value and key not user)
+                gen = (key for key, value in userdict.items() if value[0] and key not user)
                 if gen:
                     users = ', '.join([for key in gen])
                     bot_print(guess + ' is Incorrect.\n'+users+' may still have a try.')
