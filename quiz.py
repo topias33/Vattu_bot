dd09e091366eec79ae50be852e847f1e3c4a2a33
topias33 <38688365+topias33@users.noreply.github.com>
2018-05-05 22:02:05 +0300

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -5,19 +5,18 @@ def quiz_start(questions, amount = 1):
     questions = questions.split('\n')
     questions = list(filter(None, questions))
     shuffle(questions)
-    global game_questions, i
     try:
         amount = int(amount)
     except:
         amount = 1
-    return [questions[:amount], 0, amount]
+    return [questions[:amount], 0]
 
 def quiz_next(game_questions, i, answer):
     if i < len(game_questions):
         question, answer = game_questions[i].rsplit(' ? ',1)
         i += 1
-        return question
-    return ''
+        return [question, answer]
+    return []
 
 def quiz_check(guess, answer):
     whitelist = string.ascii_letters + string.digits + ' '
