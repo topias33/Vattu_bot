8a1ec9726b7200eb9a733c4731d84e6b75f0754f
topias33 <38688365+topias33@users.noreply.github.com>
2018-05-05 18:27:55 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -31,19 +31,21 @@ def translate(args):
         from_language = "auto"
         
         
-    sentence = '+'.join(args[i:])
+    sentences = '+'.join(args[i:]).split('\n')
+    textList = []
+    for sentence in sentences:
+        sentence = unicodedata.normalize('NFKD', sentence).encode('ascii','ignore').decode('utf8')
     
-    sentence = unicodedata.normalize('NFKD', sentence).encode('ascii','ignore').decode('utf8')
+        url = 'http://translate.google.com/m?hl={0:s}&sl={1:s}&q={2:s}'
+        link = url.format(to_language, from_language, sentence)
+        request = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+        web_byte = urlopen(request).read()
+        webpage = web_byte.decode('utf-8')
+        soup = BeautifulSoup(webpage, "html.parser")
+        soup = soup.find("div", {'class': 't0'})
+        textList.append(soup.get_text())
     
-    url = 'http://translate.google.com/m?hl={0:s}&sl={1:s}&q={2:s}'
-    link = url.format(to_language, from_language, sentence)
-    request = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
-    web_byte = urlopen(request).read()
-    webpage = web_byte.decode('utf-8')
-    soup = BeautifulSoup(webpage, "html.parser")
-    soup = soup.find("div", {'class': 't0'})
-    text = soup.get_text()
-    
+    text = '\n'.join(textList)
     if text:
         print('From: ' + ' '.join(args[i:]))
         print('To: ' + text)
